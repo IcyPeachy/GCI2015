09ce679d4ab7d8811fdbe64dfc22aacea0e0d5ac
IcyPeachy <theawesomerabbits@gmail.com>
2016-01-12 12:16:56 +0800

    Create lighttable.py

---
+++ lighttable.py
@@ -0,0 +1,6 @@
+print("Welcome to the coffee shop!")
+coffee_order = input("What coffee would you like?")
+if coffee_order == "latte":
+  print("That would be $5!")
+else:
+  print("Not available.")
